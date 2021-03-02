87d0729b9f313ccb3c86489e7c861dc5a97b0a35
unknown <grega.pompe1@gmail.com>
2021-03-02 17:13:32 +0100

    dodani komentarji

---
+++ comment_insert.php
@@ -0,0 +1,18 @@
+<?php
+// prikaze kaj poslje: print_r($_POST);
+include_once "session.php";
+include_once "database.php";
+
+$id = (int) $_POST['id'];
+$content = $_POST['content'];
+$user_id = $_SESSION['user_id'];
+if (!empty($id) && !empty($content)){
+    $query = "INSERT INTO comments(content,user_id,cryptocurrency_id) VALUES(?,?,?)";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$content,$user_id,$id]);
+}
+
+header("Location: cryptocurrency.php?id=$id#komentarji");
+die();
+
+?>
